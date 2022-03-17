26955fd72745ff88fd258a8998a760a429310a0d
Adam Saulters <adamsaulters@gmail.com>
2022-03-17 06:24:41 -0400

    make ctx dynamic 4

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -69,6 +69,9 @@ const Login = (props) => {
     };
   }, [emailIsValid, passwordIsValid]);
 
+
+  // 
+
   // const emailChangeHandler = (event) => {
   //   dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
 
